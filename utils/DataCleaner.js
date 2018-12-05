c904d48757536dd103950b9c30accf42fd5922ab
Alexander Ela <ela.alexander@gmail.com>
2018-12-04 18:19:44 -0700

    Add DataCleaner.js with formatCafeData function

--- utils/DataCleaner.js
+++ utils/DataCleaner.js
@@ -20,8 +20,33 @@ export const formatStationData = (stations) => {
 	    state: station.state,
 	    zip_code: station.zip_code,
 	    intersection_directions: station.intersection_directions,
-	    access_days_time: station.access_days_time
+	    access_days_time: station.access_days_time,
+	    cafes: fetchCafes(`${station.latitude},${station.longitude}`)
 		}
 	});
 	return stationPromises;
+}
+
+export const fetchCafes = (latitudeLongitude) => {
+	const url = `https://api.foursquare.com/v2/venues/search?client_id=${APIKey.cafeId}&client_secret=${APIKey.cafeSecret}&v=20180323&limit=50&ll=${latitudeLongitude}&query=coffee'&radius=1609`
+	const cafeData = await API.fetchData(url)
+	const cafeResults = formatCafeData(cafeData.response.venues)
+	return cafeResults
+}
+
+export const formatCafeData = (cafes) => {
+	const cafePromises = cafes.map(cafe => {
+		return {
+		  cafe_name: cafe.cafe_name,
+      street_address: cafe.street_address,
+      city: cafe.city,
+      state: cafe.state,
+      zip_code: cafe.zip_code,
+      cross_street: cafe.cross_street,
+      formatted_address: cafe.formatted_address,
+      distance_in_meters: cafe.distance_in_meters,
+      station_id: 
+		}
+	})
+	return cafePromises
 }

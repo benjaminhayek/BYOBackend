c74b4ec6c057396db75b4b8f56431de462059d19
Cole Hart <cole@colehart.com>
2018-12-05 11:15:58 -0700

    Add TravisCI integration

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,17 @@ module.exports = {
     },
     useNullAsDefault: true
   },
+  testing: {
+    client: 'pg',
+    connection: 'postgres://localhost/rechargeables_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,

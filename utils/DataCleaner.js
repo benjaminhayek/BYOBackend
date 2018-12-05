f4ec43f2baa92b15241b385bbb593e65136ba54c
Alexander Ela <ela.alexander@gmail.com>
2018-12-04 17:53:44 -0700

    Add DataCleaner.js with formatStationData function

--- utils/DataCleaner.js
+++ utils/DataCleaner.js
@@ -1,1 +1,27 @@
 import * as API from './API.js';
+import * as APIKey from './APIKeys.js'
+
+export const fetchStations = async (zipCode) => {
+	const url = `https://developer.nrel.gov/api/alt-fuel-stations/v1.json?access=public&status=E&fuel_type=ELEC&zip=${zipCode}&api_key=${APIKey.stationKey}&format=JSON`;
+	const stationData = await API.fetchData(url);
+	const stationResults = formatStationData(stationData.fuel_stations);
+	return stationResults;
+}
+
+export const formatStationData = (stations) => {
+	const stationPromises = stations.map(station => {
+		return {
+	    station_name: station.station_name,
+	    station_phone: station.station_phone,
+	    latitude: station.latitude,
+	    longitude: station.longitude,
+	    street_address: station.street_address,
+	    city: station.city,
+	    state: station.state,
+	    zip_code: station.zip_code,
+	    intersection_directions: station.intersection_directions,
+	    access_days_time: station.access_days_time
+		}
+	});
+	return stationPromises;
+}
